b1af899a7540b3ca8cec9866b76adb8890343781
Derek Wan <d.wan@icloud.com>
2025-05-21 21:51:45 +0900

    Commited by derekwan@DW-Mac at 2025-05-21 21:51:45 (Wed)

--- bootstrap.py
+++ bootstrap.py
@@ -38,9 +38,7 @@ def main() -> None:
     ]
 
     pre_commit_replacements = [
-        _Replacement(
-            from_="- id: run-bump-my-version", to="- id: run-bump-my-version"
-        )
+        _Replacement(from_="- id: run-bump-my-version", to="- id: run-bump-my-version")
     ]
     replacements = list(chain(template_replacements, pre_commit_replacements))
     _process_file_contents(root, replacements)
